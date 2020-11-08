cba40b78b4657886db2959987a5f0ecec90ee1a2
zqw123 <qzhang41@vols.utk.edu>
2020-11-08 18:44:18 -0500

    simple ED

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import Input_Parse
+import Market_obj as MO
+import Core
+import sys
+
+
+def market_gateway(market):
+    file = sys.argv[1]
+    Input_Parse.read(market, file)
+
+
+if __name__ == '__main__':
+    market = MO.Market('RT')
+    market_gateway(market)
+    Core.Make_Bdc(market)
+    Core.Make_PTDF(market)
+    Core.Ecnomic_dispatch(market)
+    a = 1
