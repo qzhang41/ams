1b626f41d28a263ed6e452467310cd90b4f7f3bf
zqw123 <qzhang41@vols.utk.edu>
2022-04-16 23:34:19 -0400

    examples

---
+++ load_profile_14_LTB.m
@@ -0,0 +1,36 @@
+function loadprofile = load_profile_14_LTB
+%Example load profile data file
+Type = 2;
+
+Load_level = [
+	134;
+	128;
+	14
+	125;
+	50;
+	145;
+	40;
+	55;
+	130;
+	25;
+	137;
+	42;
+];
+
+Factor = [
+0
+12.7
+19	
+-3.9
+1.6
+7.5	
+0	
+0	
+16.6
+5.8
+1.8
+1.6	
+5.8
+5
+]
+end
